99c6f4ed5b78feeca279e492671817d0c9a91f1c
TannerWyman <tannerw2002@hotmail.com>
2023-04-30 18:42:27 -0600

    year now from JS, optimized images

--- scripts/current-date.js
+++ scripts/current-date.js
@@ -1,2 +1,5 @@
 const options = {year: 'numeric', month: 'numeric', day: 'numeric', hour: '2-digit', minute: '2-digit'}
 document.getElementById('currentdate').textContent = new Date().toLocaleTimeString('en-US', options);
+
+const yearOptions = {year: 'numeric'}
+document.getElementById('currentyear').textContent = new Date().getFullYear('en-US', options);
