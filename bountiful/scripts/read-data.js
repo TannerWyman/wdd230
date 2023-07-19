0bfd6dc86e48f8562976f19e601c9109a40899c5
TannerWyman <tannerw2002@hotmail.com>
2023-07-19 17:40:24 -0600

    Final Build

--- bountiful/scripts/read-data.js
+++ bountiful/scripts/read-data.js
@@ -36,6 +36,7 @@ function logSubmit(event) {
     let name = document.getElementById('name').value;
     let mail = document.getElementById('mail').value;
     let phone = document.getElementById('phone').value
+    let special = document.getElementById('special').value
     
     let fruit1 = document.getElementById('optionsA').value;
     let fruit2 = document.getElementById('optionsB').value;
@@ -69,10 +70,25 @@ function logSubmit(event) {
 
         });
 
+        /*
         log.textContent = `Order Informaiton\n Name: ${name}\r\n Email: ${mail} <br>
-        Phone Number: ${phone}<br> First Fruit: ${fruit1}<br> Second Fruit: ${fruit2}<br>
+        Phone Number: ${phone}<br>Special Insturctions: ${special}<br> First Fruit: ${fruit1}<br> Second Fruit: ${fruit2}<br>
         Third Fruit: ${fruit3}<br>Carbohydrates: ${carbohydrates} \nProteins: ${protein}
         \nFat: ${fat} \nSugers: ${sugar} \nCalories: ${calories}`;
+        */
+       order.textContent = "Order Information"
+       orderName.textContent = "Name: " + name
+       orderMail.textContent = "Email: " + mail
+       orderPhone.textContent = "Phone: " + phone
+       orderSpecial.textContent = "Special Insturctions: " + special
+       orderFruit1.textContent = "1st Fruit: " + fruit1
+       orderFruit2.textContent = "2nd Fruit2: " + fruit2
+       orderFruit3.textContent = "3rd Fruit3: " + fruit3
+       carbs.textContent = "Carbohydrates: " + carbohydrates.toFixed(2)
+       orderProtein.textContent = "Proteins: " + protein.toFixed(2)
+       orderFat.textContent = "Fat: " + fat.toFixed(2)
+       orderSuger.textContent = "Suger: " + sugar.toFixed(2)
+       orderCalories.textContent = "Carlories: " + calories.toFixed(2)
     }
     
     getFruitData()
@@ -95,8 +111,21 @@ let protein = 0;
 let fat = 0;
 let calories = 0;
 let sugar =0;
-
-const form = document.getElementById("form");
 const log = document.getElementById("log");
-form.addEventListener("submit", logSubmit);
+const form = document.getElementById("form");
 
+const order = document.getElementById("order");
+const orderName = document.getElementById("orderName");
+const orderMail = document.getElementById("orderMail");
+const orderPhone = document.getElementById("orderPhone");
+const orderSpecial = document.getElementById("orderSpecial");
+const orderFruit1 = document.getElementById("orderFruit1");
+const orderFruit2 = document.getElementById("orderFruit2");
+const orderFruit3 = document.getElementById("orderFruit3");
+const carbs = document.getElementById("carbs");
+const orderProtein = document.getElementById("protein");
+const orderFat = document.getElementById("fat");
+const orderCalories = document.getElementById("calories");
+const orderSuger = document.getElementById("sugar");
+
+form.addEventListener("submit", logSubmit);
